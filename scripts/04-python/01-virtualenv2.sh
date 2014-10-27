e308e5be2061397ad3163b425f4e00ab68487a9a
Kevin Chen <321cyb@gmail.com>
2014-10-27 20:15:08 +0800

    remove all temp files.

--- scripts/04-python/01-virtualenv2.sh
+++ scripts/04-python/01-virtualenv2.sh
@@ -5,9 +5,11 @@ curl -O https://pypi.python.org/packages/source/v/virtualenv/virtualenv-1.11.6.t
 tar xvfz virtualenv-1.11.6.tar.gz
 cd virtualenv-1.11.6
 sudo python setup.py install
+cd ..
 sudo rm -rf virtualenv-1.11.6*
 
 
 
 # install virtual env under $HOME/virtual2/
+cd $HOME
 virtualenv virtual2
