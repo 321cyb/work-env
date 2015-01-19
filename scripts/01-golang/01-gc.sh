a2a7e0ef1b770fa7e7a93ce1a7c48c0bb1f00cba
yaboc <yabo.chen@citrix.com>
2015-01-19 10:43:33 +0800

    update golang version.

--- scripts/01-golang/01-gc.sh
+++ scripts/01-golang/01-gc.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 
-echo "installing golang 1.3.3 to /usr/local/go/"
+echo "installing golang 1.4.1 to /usr/local/go/"
 cd $HOME
-wget https://storage.googleapis.com/golang/go1.3.3.linux-amd64.tar.gz
-sudo tar -C /usr/local -xzf go1.3.3.linux-amd64.tar.gz
-rm go1.3.3.linux-amd64.tar.gz
+wget https://storage.googleapis.com/golang/go1.4.1.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.4.1.linux-amd64.tar.gz
+rm go1.4.1.linux-amd64.tar.gz
 mkdir -p gopath/src
 cd -
 
