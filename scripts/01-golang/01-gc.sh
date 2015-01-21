cb015de29fbd2066a6efa099196d7f5b7bc8b367
yaboc <yabo.chen@citrix.com>
2015-01-21 19:07:24 +0800

    add go cross compiling support.

--- scripts/01-golang/01-gc.sh
+++ scripts/01-golang/01-gc.sh
@@ -13,3 +13,8 @@ cat >> ~/.bash_profile << EOF
 export GOPATH=$HOME/gopath/
 export PATH=\$PATH:/usr/local/go/bin:$HOME/gopath/bin/
 EOF
+
+# add support for cross compiling.
+source ~/.bash_profile
+go get github.com/laher/goxc
+sudo /home/yaboc/gopath/bin/goxc -t
